c0ba7962fb1aba31f91438cbbfb7dc03d62ef65d
Chandrahas Kasoju <kasoju.chandrahas@gmail.com>
2026-06-10 18:00:23 +0200

    change accent

--- body_bringup/speech_test.py
+++ body_bringup/speech_test.py
@@ -11,7 +11,7 @@ import time
 # --- Configuration ---
 PIPER_EXECUTABLE  = "piper"
 # Updated to use a standard Danish Piper TTS model
-PIPER_MODEL_PATH  = "/root/piper-tts/da_DK/talesyntese-medium.onnx" 
+PIPER_MODEL_PATH  = "/root/piper-tts/en_GB-jenny_dioco-medium.onnx" 
 # Translated to Danish
 #SPEECH_TEXT       = "Hej, jeg er Tely. Tryk venligst på knappen på skærmen for at få hjælp."
 SPEECH_TEXT = "Hi, I am Tele. Please press the button on the screen for assistance" 
